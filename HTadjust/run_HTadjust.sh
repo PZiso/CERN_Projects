ad5e2fda217f23589904a4ab18912a070df8a674
Panagiotis Zisopoulos <panagiotis.zisopoulos@cern.ch>
2018-09-19 06:28:42 +0000

    Run script for HTadjust (CO Python 3 Distribution)

---
+++ HTadjust/run_HTadjust.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# source /acc/local/share/python/L867/setup.sh # Use Python 3 Officialy supported by CO (Attention: The L867 subdir depends on the CPU of the running machine. Try L866 if this doesn't work.)
+
+source /acc/local/share/python/L866/setup.sh
+
+python HTadjust.py & # run the module in the background
