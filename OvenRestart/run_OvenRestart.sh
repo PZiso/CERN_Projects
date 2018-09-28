f5cad0b315adb9a07853489cfd93dc20dd84f4f8
Panagiotis Zisopoulos <panagiotis.zisopoulos@cern.ch>
2018-09-28 08:56:37 +0000

    Add new file

---
+++ OvenRestart/run_OvenRestart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# source /acc/local/share/python/L867/setup.sh # Use Python 3 Officialy supported by CO (Attention:
+#The L867 subdir depends on the CPU of the running machine. Try L866 if this doesn't work.)
+
+source /acc/local/share/python/L866/setup.sh
+
+python OvenRestart.py & # run the module in the background. Remove & for shell print.
+
+
