fa063ae48550e1cd312c2d75fd6449af57e8ac03
Panagiotis Zisopoulos <panagiotis.zisopoulos@cern.ch>
2018-10-15 09:39:08 +0000

    Update run_OvenRestart.sh

--- OvenRestart/run_OvenRestart.sh
+++ OvenRestart/run_OvenRestart.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# source /acc/local/share/python/L867/setup.sh # Use Python 3 Officialy supported by CO (Attention:
-#The L867 subdir depends on the CPU of the running machine. Try L866 if this doesn't work.)
-
-source /acc/local/share/python/L866/setup.sh
-
-python OvenRestart.py & # run the module in the background. Remove & for shell print.
 
 
+ssh -f ln3op@cwe-513-vol262 \
+"cd /user/ln3op/GHOST/OvenRestart/src/ && source /acc"\
+"/local/share/python/L866/setup.sh && nohup python /user/ln3op/GHOST/"\
+"OvenRestart/src/OvenRestart.py > garbage.in 2> garbage.out < /dev/null &"
+
+# Note the Line continuations before messing with the commands ;) 
+
+
+
+# run the module in the background. Remove & for shell print.
+
+# # source /acc/local/share/python/L867/setup.sh # Use Python 3 Officialy supported by CO (Attention: The L867 subdir depends on the CPU of the running machine. Try L866 if this doesn't work.)
+
+# # # Send everything to /dev/null so the program gets executed in the background-Use nohuup for no hungup
